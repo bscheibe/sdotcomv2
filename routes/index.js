3d5b0b882c5ee47b698aa197a15092d2f0c65a02
Brent Scheibelhut <brent@roadmunk.com>
2014-06-25 17:30:56 -0400

    Rough draft of the layout still researching

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Roadmunk' });
+});
+
+var homeData = {
+	title : "Roadmunk Landing Page",
+	description : "Create Brilliant Roadmaps with Roadmunk",
+	author : "Brent Scheibelhut",
+	content : "home",
+	filename: true
+}
+
+/* GET test page. */
+router.get('/test', function(req, res) {
+  res.render('layout', homeData);
 });
 
 module.exports = router;
