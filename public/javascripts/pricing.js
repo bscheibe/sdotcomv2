9691da9e4acf07dc0dffdeff0982b494347f23bf
Brent Scheibelhut <brent@roadmunk.com>
2014-07-17 15:43:30 -0400

    Added functionality to the toggle

--- public/javascripts/pricing.js
+++ public/javascripts/pricing.js
@@ -7,24 +7,46 @@ var business_annually = "19";
 var professional_monthly = "61";
 var professional_annually = "49";
 
-$('#monthly').on("click", function() {
-	$("#monthly").removeClass("grey");
+function monthly () {
+    $("#monthly").removeClass("grey");
+    $("#monthly").addClass("selected");
     $("#annually").removeClass("right");
+    $("#annually").removeClass("selected");
     $(".switch-pill").removeClass("right");
 
     $("#starterPrice").html(starter_monthly);
     $("#businessPrice").html(business_monthly);
     $("#professionalPrice").html(professional_monthly);
     $(".subheader2").html("billed monthly");
-});
+}
 
-$('#annually').on("click", function() {
+function annually () {
     $("#annually").addClass("right");
+    $("#annually").addClass("selected");
     $("#monthly").addClass("grey");
+    $("#monthly").removeClass("selected");
     $(".switch-pill").addClass("right");
 
     $("#starterPrice").html(starter_annually);
     $("#businessPrice").html(business_annually);
     $("#professionalPrice").html(professional_annually);
     $(".subheader2").html("billed annually");
+}
+
+$('#toggle').on('click', function () {
+
+    if ($('.selected').attr("id") == "monthly") {
+        annually();
+    } else {
+        monthly();
+    }
+
+});
+
+$('#monthly').on("click", function() {
+	monthly();
+});
+
+$('#annually').on("click", function() {
+    annually();
 });
