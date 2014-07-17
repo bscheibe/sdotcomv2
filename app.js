a80a28fd010610173cd84fcbf6378ef57f59ecc5
Brent Scheibelhut <brent@roadmunk.com>
2014-07-17 13:39:02 -0400

    Fixed error

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var limits       = require('limits');
 var minify       = require('express-minify');
 
 var routes = require('./routes/index');
-var users  = require('./routes/users');
 
 var app = express();
 
@@ -37,7 +36,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
